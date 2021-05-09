3487c6455dd67e0d7bb9b2b5adfe5a8c93f8f452
Rik Vandenheede <Rik.Vandenheede0@hotmail.com>
2021-05-09 16:23:33 +0200

    log in systeem bijna klaar

--- Js/login.js
+++ Js/login.js
@@ -1,55 +1,51 @@
-/*
-function User(voornaam, achternaam, gebruikersnaam, wachtwoord, email){
-        this.voornaam = voornaam;
-        this.achternaam = achternaam;
-        this.gebruikersnaam = gebruikersnaam;
-        this.wachtwoord = wachtwoord;
-        this.email = email;
-}
-
-let arisha = new User("Arisha", "Mezienova", "arishaM", "test1", "...@student.pxl.be");
-let kai = new User("Kai", "Meyers", "KaiM", "test2", "...@student.pxl.be");
-let thymen = new User("Thymen", "Willemsen", "ThymenW", "test3", "...@student.pxl.be");
-let dries = new User("Dries", "Roefs", "DriesR", "test4", "...@student.pxl.be");
-let rik = new User("Rik", "Vandenheede", "RikV", "test5", "...@student.pxl.be");
-
-let MyArr = [arisha, kai, thymen, dries, rik];
-
-sessionStorage.setItem("user", JSON.stringify(MyArr[4]));
-console.log(JSON.parse(sessionStorage.getItem("user")));
-*/
-
 let userId = 0;
+const naam = document.getElementById("naamUser");
+const telefoon = document.getElementById("telefoonUser");
+const mail = document.getElementById("mailUser");
+const wachtwoord = document.getElementById("wachtwoordUser");
+const straatNr = document.getElementById("straatUser");
+const postcode = document.getElementById("postcodeUser");
+
+const mailLogIn = document.getElementById("mailInput");
+const wachtwoordLogIn = document.getElementById("wachtwoordInput");
+const mailPlusWachtwoord = mailLogIn.value + wachtwoordLogIn.value;
+let dataString = "";
 
 document.getElementById("registreer").addEventListener("click", registreer);
 document.getElementById("logIn").addEventListener("click", logIn);
 
 function registreer(){
-    let naam = document.getElementById("naamUser").value;
-    let telefoon = document.getElementById("telefoonUser").value;
-    let mail = document.getElementById("mailUser").value;
-    let wachtwoord = document.getElementById("wachtwoordUser").value;
-    let straatNr = document.getElementById("straatUser").value;
-    let postcode = document.getElementById("postcodeUser").value;
-
     function User(naam, telefoon, mail, wachtwoord, straatNr, postcode){
-        this.naam = naam;
-        this.telefoon = telefoon;
-        this.mail = mail;
-        this.wachtwoord = wachtwoord;
-        this.straatNr = straatNr;
-        this.postcode = postcode;
+        this.naam = naam.value;
+        this.telefoon = telefoon.value;
+        this.mail = mail.value;
+        this.wachtwoord = wachtwoord.value;
+        this.straatNr = straatNr.value;
+        this.postcode = postcode.value;
     }
-    let user = new User(naam, telefoon, mail, wachtwoord, straatNr, postcode);
+    const user = new User(naam, telefoon, mail, wachtwoord, straatNr, postcode);
 
+    if(userId < sessionStorage.length -1){
+        for(let i = 0; i < sessionStorage.length -1; i++){
+            userId++;
+        }
+    }
     sessionStorage.setItem("user" + userId, JSON.stringify(user));
     userId++;
 }
+
 function logIn(){
-    let mailLogIn = document.getElementById("mailInput").value;
-    let wachtwoordLogIn = document.getElementById("wachtwoordInput").value;
-    for(let i = 0; i < sessionStorage.length; i++){
-        if(mailLogIn === JSON.parse(sessionStorage.getItem("user"+i)) && wachtwoordLogIn === JSON.parse(sessionStorage.getItem("user"+i))){
+    for(let i = 0; i< sessionStorage.length /*- 1*/; i++){
+        let jsonData = sessionStorage.getItem("user" + i);
+        let parsedJson = JSON.parse(jsonData);
+        for(key in parsedJson){
+            if(parsedJson[key] === mailLogIn){
+                dataString += parsedJson[key];
+            }else if(parsedJson[key] === wachtwoordLogIn){
+                dataString += parsedJson[key];
+            }
+        }
+        if(mailPlusWachtwoord === dataString){
             window.location = "./index.html";
         }
     }
