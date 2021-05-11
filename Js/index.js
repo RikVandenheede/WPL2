bafa527a7594f2e05988649a6eb13ffe4bca6a9f
RikVandenheede <rik.vandenheede0@hotmail.com>
2021-05-11 17:01:14 +0200

    footer add

--- Js/index.js
+++ Js/index.js
@@ -1,7 +1,7 @@
  function loggedIn(naam){
     if(sessionStorage.length !== 1){
-        WelcomeName = naam.charAt(0).toUpperCase() + naam.substring(1);
-        return document.getElementById("naam").innerHTML = WelcomeName;
+        let welcomeName = naam.charAt(0).toUpperCase() + naam.substring(1);
+        document.getElementById("naam").innerHTML = welcomeName;
     }
 }
 loggedIn(sessionStorage.getItem("showName"));
