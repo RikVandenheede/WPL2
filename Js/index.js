bfc125a60bf63290716f3009a34e74e75bbc4075
RikVandenheede <rik.vandenheede0@hotmail.com>
2021-05-18 16:22:18 +0200

    yeep

--- Js/index.js
+++ Js/index.js
@@ -5,10 +5,11 @@ let mountain = document.getElementById("mountain");
 let beach = document.getElementById("beach");
 let land = document.getElementById("land");
 
+/*
 window.addEventListener("scroll", () => {
     let scroll = this.scrollY;
         bovenlqqg.style.top =  (scroll * 0.3) + 2260 + "px";
         mountain.style.top = (scroll * 0.25) + 2490 + "px";
         beach.style.top =  (scroll * 0.15) + 3200 + "px";
     console.log(scroll);
-});
+});*/
