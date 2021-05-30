4149fbdff5aa9d26eafff482400d029b8c0a527f
Rik Vandenheede <Rik.Vandenheede0@hotmail.com>
2021-05-30 19:27:33 +0200

    animations over ons

--- js/over_ons.js
+++ js/over_ons.js
@@ -1,1 +1,24 @@
 "use strict";
+
+let animationKai = document.querySelectorAll(".color-odd")[0];
+let animationDries = document.querySelectorAll(".color-odd")[1];
+let animationRik = document.querySelectorAll(".color-even")[0];
+let animationThymen = document.querySelectorAll(".color-even")[1];
+let animationArisha = document.querySelectorAll(".color-even")[2];
+
+
+window.addEventListener("scroll", () => {
+    let scroll = this.scrollY;
+    if(scroll > 100){
+        animationRik.classList.add("color-even-ani");
+    }if(scroll > 500){
+        animationKai.classList.add("color-odd-ani");
+    }if(scroll > 900){
+        animationThymen.classList.add("color-even-ani");
+    }if(scroll > 1300){
+        animationDries.classList.add("color-odd-ani");
+    }if(scroll > 1700){
+        animationArisha.classList.add("color-even-ani");
+    }
+    console.log(scroll);
+});
