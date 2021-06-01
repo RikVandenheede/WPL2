744ef1f45e1a23905d677456bd25ef276cf705c2
ThymenWillemsenPXL <thymen.willemsen@student.pxl.be>
2021-06-01 09:02:13 +0200

    animatie aanpassing

--- js/over_ons.js
+++ js/over_ons.js
@@ -9,15 +9,15 @@ let animationArisha = document.querySelectorAll(".color-even")[2];
 
 window.addEventListener("scroll", () => {
     let scroll = this.scrollY;
-    if(scroll > 100){
+    if(scroll > 300){
         animationRik.classList.add("color-even-ani");
-    }if(scroll > 500){
+    }if(scroll > 700){
         animationKai.classList.add("color-odd-ani");
-    }if(scroll > 900){
+    }if(scroll > 1100){
         animationThymen.classList.add("color-even-ani");
-    }if(scroll > 1300){
+    }if(scroll > 1500){
         animationDries.classList.add("color-odd-ani");
-    }if(scroll > 1700){
+    }if(scroll > 1900){
         animationArisha.classList.add("color-even-ani");
     }
     console.log(scroll);
